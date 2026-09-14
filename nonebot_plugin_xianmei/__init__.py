660f39f192fe3c731317ff74a91ca65c665e17fb
liangpuyue <plia0974@uni.sydney.edu.au>
2026-09-14 11:16:39 +0800

    docs: 插件用法说明同步三连播报行为

--- nonebot_plugin_xianmei/__init__.py
+++ nonebot_plugin_xianmei/__init__.py
@@ -4,7 +4,7 @@ __plugin_meta__ = PluginMetadata(
     name="桃子献媚",
     description="向本群群主（B站主播小小桃纸哟）自动献媚的娱乐插件",
     usage=(
-        "群主在群里出现时自动发送一条彩虹屁；\n"
+        "群主在群里出现时先播报再自动献媚一条彩虹屁；\n"
         "superuser 命令：/献媚设置群主 /献媚设置上限 /献媚设置冷却 /献媚开关 /献媚状态 /献媚预览"
     ),
     type="application",
